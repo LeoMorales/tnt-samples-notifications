5cf82cdd101c9f15d42e54177e89e5aaf25b0f28
Leonardo Morales <leo.moralesr23@gmail.com>
2021-06-02 10:36:12 -0300

    first commit

---
+++ app/src/main/java/unpsjb/ing/tnt/notificationssample/MainActivity.kt
@@ -0,0 +1,38 @@
+package unpsjb.ing.tnt.notificationssample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.app.NotificationCompat
+import androidx.databinding.DataBindingUtil
+import unpsjb.ing.tnt.notificationssample.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val CHANNEL_ID: String = "CHANNEL-01"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding: ActivityMainBinding  = DataBindingUtil.setContentView(
+            this, R.layout.activity_main)
+
+        createNotificationChannel()
+        binding.buttonClickme.setOnClickListener {
+            handleButtonClick()
+        }
+    }
+
+    private fun createNotificationChannel() {
+        TODO("Implementar la creación del canal")
+    }
+
+    private fun handleButtonClick() {
+        // construir la notificación y enviarla:
+        var builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("Titulo de la notificación")
+            .setContentText("Contenido del cuerpo de la notificación")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+    }
+
+}
